56cb3c6d54c491ca0d3665074b14b05a9662d565
Saju Thankappan <sajuthankappan@gmail.com>
2022-10-26 16:53:54 +0530

    Card example

---
+++ src/lib/Card.svelte
@@ -0,0 +1,35 @@
+<script>
+  let header;
+</script>
+
+
+<div class="card-container">
+  <div class="header">
+    <slot name="header" />
+  </div>
+
+  <div class="main">
+    <slot />
+  </div>
+
+  <div class="footer">
+    <slot name="footer" />
+  </div>
+</div>
+  
+<style>
+  .header {
+    background-color: gray;
+    padding: .25rem 0;
+  }
+
+  .footer {
+    background-color: lightgreen;
+    padding: .25rem 0;
+  }
+
+  .card-container {
+    border: solid 1px;
+    padding: 1rem;
+  }
+</style>
--- src/lib/HelloCompositionParent.svelte
+++ src/lib/HelloCompositionParent.svelte
@@ -1,10 +1,15 @@
 <script>
-    import HelloCompositionChild from "./HelloCompositionChild.svelte";
+    import Card from "./Card.svelte";
 </script>
 
 <h1>Composition Example</h1>
 
-<HelloCompositionChild>
-  <button>Ok</button>
-  <input type="text" />
-</HelloCompositionChild>
+<Card>
+  <p >Card Content here..</p>
+
+  <p slot="footer">Footer</p>
+
+  <div slot="header">
+    <h2>Card Header New</h2>
+  </div>
+</Card>
